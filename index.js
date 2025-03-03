7fe8152167b08b0b7e5253bd8e90a57dd3d766f3
Jainam <jainamrupani04@gmail.com>
2025-03-03 20:06:08 +0530

    Modal Section

---
+++ index.js
@@ -0,0 +1,7 @@
+const state = {
+      taskList: []
+};
+
+const taskContent = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+const htmlTaskContent = ({ id, title, type, description, url }) => { };

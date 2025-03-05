9531ebb54dba04fdfe175a52469ad412a47c5e35
Jainam <jainamrupani04@gmail.com>
2025-03-05 22:18:11 +0530

    Cards added to localstorage and showed on ui

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const state = {
 const taskContent = document.querySelector(".task__contents");
 const taskModal = document.querySelector(".task__modal__body");
 const htmlTaskContent = ({ id, title, type, description, url }) => `
-      <div class='col-md-6 col-lg-3 mt-3' id=${id}>
+      <div class='col-md-6 col-lg-3 my-3' id=${id}>
             <div class='card shadow-sm task__card'>
                   <div class='card-header d-flex justify-content-end task__card__header'>
-                        <button type='button' class='btn btn-outline-primary mr-1.5' name=${id}>
+                        <button type='button' class='btn btn-outline-primary m-1' name=${id}>
                               <i class='fas fa-pencil-alt' name=${id}></i>
                         </button>
-                        <button type='button' class='btn btn-outline-danger mr-1.5' name=${id}>
+                        <button type='button' class='btn btn-outline-danger m-1' name=${id}>
                               <i class='fas fa-trash-alt' name=${id}></i>
                         </button>
                   </div>
                   <div class='card-body'>
-                        ${url && 
-                              `<img width='100%' src=${url} alt='Card Image' class='card-img-top mb-3 rounded-lg' />`
-                        }
+                        ${url &&
+      `<img width='100%' src=${url} alt='Card Image' class='card-img-top mb-3 rounded-lg' />`
+      }
                         <h4 class='card-title task__card__title'>${title}</h4>
                         <p class='task__card__description trim-3-lines text-muted'>${description}</p>
                         <div class='tags text-white d-flex flex-wrap'>
@@ -36,10 +36,9 @@ const htmlModalContent = ({ id, title, description, url }) => {
       const date = new Date(parseInt(id));
       return `
             <div id=${id}>
-                  ${
-                        url && 
-                        `<img width='100%' src=${url} alt='Card Image' class='img-fluid place__holder__image mb-3' />`
-                  }
+                  ${url &&
+            `<img width='100%' src=${url} alt='Card Image' class='img-fluid place__holder__image mb-3' />`
+            }
                   <strong class='text-muted text-sm'>Created on: ${date.toDateString()}</strong>
                   <h2 class='my-3'>${title}</h2>
                   <p class='text-muted'>${description}</p>
@@ -55,3 +54,29 @@ const updateLocalStorage = () => {
             })
       );
 };
+
+const loadInitialData = () => {
+      var localStorageCopy = JSON.parse(localStorage.task);
+      if (localStorageCopy) {
+            state.taskList = localStorageCopy.tasks;
+            state.taskList.map((cardDate) => {
+                  taskContent.insertAdjacentHTML("beforeend", htmlTaskContent(cardDate));
+            });
+      }
+}
+
+const handleSubmit = (event) => {
+      const id = `${Date.now()}`;
+      const input = {
+            url: document.getElementById('imageUrl').value,
+            title: document.getElementById('taskTitle').value,
+            type: document.getElementById('tags').value,
+            description: document.getElementById('taskDescription').value
+      };
+      if (input.title == "" || input.type == "" || input.description == "") {
+            return alert("Please fill all the necessary fields!");
+      }
+      taskContent.insertAdjacentHTML("beforeend", htmlTaskContent({ ...input, id }));
+      state.taskList.push({ ...input, id });
+      updateLocalStorage();
+}
